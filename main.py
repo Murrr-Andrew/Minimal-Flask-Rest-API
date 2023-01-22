bd60a485dc2666e2323d8118bee62e9988c10c0a
Andrew Murrow <murrr.andrew@gmail.com>
2023-01-22 20:29:09 +0200

    Flask: Rework POST method and adding datetime field

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask
 from flask_restful import Resource, Api, reqparse, abort
 
@@ -9,8 +10,14 @@ parser = reqparse.RequestParser()
 parser.add_argument('title', required=True)
 
 videos = {
-    'video1': {'title': 'Hello World in Python'},
-    'video2': {'title': 'Why Matlab is the best language ever'}
+    '1': {
+        'title': 'Hello World in Python',
+        'uploadDate': '2023-01-22 16:25:01'
+    },
+    '2': {
+        'title': 'Why Matlab is the best language ever',
+        'uploadDate': '2023-01-22 17:15:25'
+    }
 }
 
 
@@ -22,34 +29,23 @@ class Video(Resource):
         """
         GET method for Api requests
         """
-        if video_id == 'all':
-            return videos, 200
-        elif video_id not in videos:
-            abort(http_status_code=404, message=f'Video {video_id} not found!')
+        if video_id not in videos:
+            abort(http_status_code=404, message=f'Video with ID {video_id} not found!')
         else:
             return videos[video_id], 200
-
-    def post(self, video_id):
-        """
-        POST method for Api requests
-        """
-        if video_id in videos:
-            abort(http_status_code=409, message=f'Video with {video_id} id already exists')
-        else:
-            args = parser.parse_args()
-            new_video = {'title': args['title']}
-            videos[video_id] = new_video
-            return {video_id: videos[video_id]}, 200
 
     def put(self, video_id):
         """
         PUT method for Api requests
         """
         if video_id not in videos:
-            abort(http_status_code=40, message=f'Video {video_id} not found!')
+            abort(http_status_code=40, message=f'Video with ID {video_id} not found!')
         else:
             args = parser.parse_args()
-            new_video = {'title': args['title']}
+            new_video = {
+                'title': args['title'],
+                'uploadDate': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            }
             videos[video_id] = new_video
             return {video_id: videos[video_id]}, 200
 
@@ -58,14 +54,40 @@ class Video(Resource):
         DELETE method for Api request
         """
         if video_id not in videos:
-            abort(http_status_code=404, message=f'Video {video_id} not found!')
+            abort(http_status_code=404, message=f'Video with ID {video_id} not found!')
         else:
             videos.pop(video_id)
             return {'message': 'Video has been deleted'}, 200
 
 
+class VideoSchedule(Resource):
+    """
+    Video Schedule class for Api
+    """
+    def get(self):
+        """
+        GET method for Api requests
+        """
+        return videos, 200
+
+    def post(self):
+        """
+        POST method for Api requests
+        """
+        args = parser.parse_args()
+        video_id = max([int(key) for key in videos.keys()]) + 1
+        new_video = {
+            'title': args['title'],
+            'uploadDate': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        }
+        videos[video_id] = new_video
+
+        return {video_id: videos[video_id]}, 200
+
+
 # Adding resource to the API and the endpoint for it
 api.add_resource(Video, '/videos/<video_id>')
+api.add_resource(VideoSchedule, '/videos')
 
 if __name__ == '__main__':
     app.run()
