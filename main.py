72a249f2c751923dd1ed859eb181c0469786c75d
Andrew Murrow <murrr.andrew@gmail.com>
2023-01-22 22:28:47 +0200

    Flask: Refactor

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import json
+import os
 from datetime import datetime
 from flask import Flask
-from flask_restful import Resource, Api, reqparse, abort
-
+from flask_restful import Resource, Api, reqparse
 
 app = Flask('VideoAPI')
 api = Api(app)
@@ -9,83 +10,125 @@ api = Api(app)
 parser = reqparse.RequestParser()
 parser.add_argument('title', required=True)
 
-videos = {
-    '1': {
-        'title': 'Hello World in Python',
-        'uploadDate': '2023-01-22 16:25:01'
-    },
-    '2': {
-        'title': 'Why Matlab is the best language ever',
-        'uploadDate': '2023-01-22 17:15:25'
-    }
-}
-
 
 class Video(Resource):
     """
     Video class for Api
     """
+    file = os.path.join(os.getcwd(), 'videos.json')
+
     def get(self, video_id):
         """
         GET method for Api requests
         """
-        if video_id not in videos:
-            abort(http_status_code=404, message=f'Video with ID {video_id} not found!')
-        else:
-            return videos[video_id], 200
+        try:
+            with open(self.file) as f:
+                videos = json.load(f)
+
+            if video_id not in videos:
+                response = {'message': f'Video with ID {video_id} not found!'}, 404
+            else:
+                response = videos[video_id], 200
+
+        except FileNotFoundError:
+            response = {'message': 'Server error. Try again later!'}, 500
+
+        return response
 
     def put(self, video_id):
         """
         PUT method for Api requests
         """
-        if video_id not in videos:
-            abort(http_status_code=40, message=f'Video with ID {video_id} not found!')
-        else:
-            args = parser.parse_args()
-            new_video = {
-                'title': args['title'],
-                'uploadDate': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            }
-            videos[video_id] = new_video
-            return {video_id: videos[video_id]}, 200
+        try:
+            with open(self.file) as f:
+                videos = json.load(f)
+
+            if video_id not in videos:
+                response = {'message': f'Video with ID {video_id} not found!'}, 404
+            else:
+                args = parser.parse_args()
+                new_video = {'title': args['title'], 'uploadDate': datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+                videos[video_id] = new_video
+
+                with open(self.file, 'w') as f:
+                    json.dump(videos, f)
+
+                response = {video_id: videos[video_id]}, 200
+
+        except FileNotFoundError:
+            response = {'message': 'Server error. Try again later!'}, 500
+
+        return response
 
     def delete(self, video_id):
         """
         DELETE method for Api request
         """
-        if video_id not in videos:
-            abort(http_status_code=404, message=f'Video with ID {video_id} not found!')
-        else:
-            videos.pop(video_id)
-            return {'message': 'Video has been deleted'}, 200
+        try:
+            with open(self.file, 'r') as f:
+                videos = json.load(f)
+
+            if video_id not in videos:
+                response = {'message': f'Video with ID {video_id} not found!'}, 404
+            else:
+                videos.pop(video_id)
+
+                with open(self.file, 'w') as f:
+                    json.dump(videos, f)
+
+                response = {'message': 'Video has been deleted'}, 200
+
+        except FileNotFoundError:
+            response = {'message': 'Server error. Try again later!'}, 500
+
+        return response
 
 
 class VideoSchedule(Resource):
     """
     Video Schedule class for Api
     """
+    file = os.path.join(os.getcwd(), 'videos.json')
+
     def get(self):
         """
         GET method for Api requests
         """
-        return videos, 200
+        try:
+            with open(self.file) as f:
+                videos = json.load(f)
+
+            response = videos, 200
+
+        except FileNotFoundError:
+            response = {'message': 'Server error. Try again later!'}, 500
+
+        return response
 
     def post(self):
         """
         POST method for Api requests
         """
-        args = parser.parse_args()
-        video_id = max([int(key) for key in videos.keys()]) + 1
-        new_video = {
-            'title': args['title'],
-            'uploadDate': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        }
-        videos[video_id] = new_video
+        try:
+            with open(self.file) as f:
+                videos = json.load(f)
 
-        return {video_id: videos[video_id]}, 200
+            args = parser.parse_args()
+            video_id = max([int(key) for key in videos.keys()]) + 1
+            new_video = {'title': args['title'], 'uploadDate': datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+            videos[video_id] = new_video
+
+            with open(self.file, 'w') as f:
+                json.dump(videos, f)
+
+            response = {video_id: videos[video_id]}, 200
+
+        except FileNotFoundError:
+            response = {'message': 'Server error. Try again later!'}, 500
+
+        return response
 
 
-# Adding resource to the API and the endpoint for it
 api.add_resource(Video, '/videos/<video_id>')
 api.add_resource(VideoSchedule, '/videos')
 
