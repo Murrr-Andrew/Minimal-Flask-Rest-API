e3b6a995b841f9b68a2e6b309e5b5de3337306c8
Andrew Murrow <murrr.andrew@gmail.com>
2023-01-22 16:24:42 +0200

    Flask: Init

---
+++ help.py
@@ -0,0 +1,14 @@
+# === GIT =======================================
+# git config user.email "myemail@gmail.com"
+# git config user.email
+
+# === VENV ======================================
+# python3 -m venv venv -> installing virtual environment
+# source venv/bin/activate -> activating virtual environment
+# deactivate -> deactivate virtual environment
+
+# === REQUIREMENTS ==============================
+# pip3 freeze > requirements.txt
+
+# === FLASK =====================================
+# pip3 install flask
